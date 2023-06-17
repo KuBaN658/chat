e9d48af1414797bdc3ed3984e5b9d1171de890e0
KuBaN23_ <garkovski_dmitri@mail.ru>
2023-06-17 16:34:18 +0300

    создание домашней страницы

--- chat/chat_usb/views.py
+++ chat/chat_usb/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView
+from .models import User, Message, Chat
 
 
-def chat(request):
-    return HttpResponse('Здесь будет чат')
-
+def index(request):
+    """
+    Функция отображения добашней страницы сайта
+    """
+    num_messages = Message.objects.all().count()
+    num_user = User.objects.all().count()
+    num_chat = Chat.objects.all().count()
+    return render(
+        request,
+        'index.html',
+        context={
+            'num_messages': num_messages,
+            'num_user': num_user,
+            'num_chat': num_chat,
+        }
+    )
